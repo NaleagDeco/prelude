4724f6ce6e6aa36cb8c6dfdac80d6e4986cb0990
Gaelan D'costa <gdcosta@gmail.com>
2013-09-17 13:05:31 -0400

    Adding all my prelude customizations.

---
+++ personal/smartparens-paredit.el
@@ -0,0 +1,19 @@
+;;; smartparens-paredit.el --- Make SmartParens act more like Paredit
+;;
+;; Copyright © 2013 Gaelan D'costa
+;;
+
+;; Author: Gaelan D'costa <gdcosta@gmail.com>
+;; URL: https://github.com/NaleagDeco/prelude
+;; Created: August 2013
+;; Keywords: convenience
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; I prefer some of the paredit behaviour, so let's keep that.
+
+;;; Code:
+(smartparens-strict-mode t)
+;;; smartparens-paredit.el ends here
