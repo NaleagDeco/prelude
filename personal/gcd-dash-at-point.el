85dd575bd8c5c6331494544dcd555782ee498b44
Gaelan D'costa <gdcosta@gmail.com>
2013-10-28 23:00:09 -0400

    Add integration for OS X documentation app Dash

---
+++ personal/gcd-dash-at-point.el
@@ -0,0 +1,21 @@
+;;; gcd-dash-at-point.el -- Emacs Prelude (Personal): Integrate with Dash app.
+;;
+;; Copyright © 2013 Gaelan D'costa
+;;
+;; Author: Gaelan D'costa <gdcosta@gmail.com>
+;; URL: https://github.com/NaleagDeco/prelude
+;; Created: October 2013
+;; Keywords: convenience
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; I use the OS X app Dash to browse developer documentation, we should enable
+;; Emacs integration
+
+;;; Code:
+(prelude-require-package 'dash-at-point)
+
+(provide 'gcd-dash-at-point)
+;;; gcd-dash-at-point.el ends here
