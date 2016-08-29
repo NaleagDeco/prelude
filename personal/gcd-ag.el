df66cb547d97e4b28112a752deb5cd7179754f6e
Gaelan D'costa <gaelan@tulip.io>
2016-08-28 20:36:31 -0400

    Add ag (silver searcher) support

---
+++ personal/gcd-ag.el
@@ -0,0 +1,21 @@
+;;; gcd-ag.el --- Emacs Prelude (Personal): clj-refactor support
+;;
+;; Copyright © 2016 Gaelan D'costa
+;;
+;; Author: Gaelan D'costa <gdcosta@gmail.com>
+;; URL: https://github.com/robotdisco/prelude
+;; Created: August 2016
+;; Keywords: convenience
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; Install supprot for the "silver searcher" grep replacement, "ag".
+
+;;; Code:
+
+(prelude-require-package 'ag)
+
+(provide 'ag)
+;;; gcd-ag.el ends here
